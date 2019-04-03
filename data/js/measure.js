9ea61ef6954794cd75118fb743b8337993aa39d2
SardorAllaberganov <sardorallaberganov96@gmail.com>
2019-04-03 12:27:35 +0500

    body page

--- data/js/measure.js
+++ data/js/measure.js
@@ -1,14 +1,12 @@
 app.controller('measurementController', function($scope, $http) {
-  $(document).on('load', function() {
-    $('#\\#dialog_measure_sizes').modal('show');
-  });
+
   $('.inputs').keyup(function() {
     if ($(this).val().length == $(this).attr("maxlength")) {
       $(this).closest('div').next().find('.inputs').focus();
     }
   });
   $scope.measure = () =>{
-    $http.get("/shirtShop.github.io/data/js/shirt-sizes.json").then(function(size_data){
+    $http.get("z/data/js/shirt-sizes.json").then(function(size_data){
       $scope.size = angular.fromJson(size_data);
       for(let i = 0; i < $scope.size.data.length;i++) {
         // console.log($scope.size.data[i].Id);
