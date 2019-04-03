5063bb5597eeeaa417153c92c551d16a244c5108
SardorAllaberganov <sardorallaberganov96@gmail.com>
2019-04-03 12:00:25 +0500

    body page

--- data/js/measure.js
+++ data/js/measure.js
@@ -8,7 +8,7 @@ app.controller('measurementController', function($scope, $http) {
     }
   });
   $scope.measure = () =>{
-    $http.get("/data/js/shirt-sizes.json").then(function(size_data){
+    $http.get("/shirtShop.github.io/data/js/shirt-sizes.json").then(function(size_data){
       $scope.size = angular.fromJson(size_data);
       for(let i = 0; i < $scope.size.data.length;i++) {
         // console.log($scope.size.data[i].Id);
